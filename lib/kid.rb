076040401ff144c09869139fbe6f3e776f572f42
Heather Moreira <moreiraheather@gmail.com>
2020-04-10 02:59:14 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,7 +3,9 @@ require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
 
 class Kid
+  include Dance 
   attr_accessor :name 
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
+  
 end
